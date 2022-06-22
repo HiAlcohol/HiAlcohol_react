bb323d3a7674f975c17e8c67ce25dd044469d37a
alreadynyeong <kjw87130@naver.com>
2022-06-22 16:25:11 +0900

    #3 임시 헤더에 링크 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className='Header'>
-            Hi Alcohol
+            <Link to='/'>Hi Alcohol</Link>
         </div>
     );
 };
