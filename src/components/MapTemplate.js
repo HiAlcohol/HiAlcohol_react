60d3d98be879dd3308736fb65eb071389034b61d
alreadynyeong <kjw87130@naver.com>
2022-06-22 18:21:31 +0900

    #4 카카오 지도 추가

--- src/components/MapTemplate.js
+++ src/components/MapTemplate.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import  React, { useEffect} from "react";
 import './MapTemplate.scss';
+const { kakao } = window;
 
 const MapTemplate = () => {
+    useEffect(() => {
+
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 3,
+        };
+        const map = new kakao.maps.Map(container, options);
+      }, []);
+    
     return (
         
         <div className='MapTemplate'>
