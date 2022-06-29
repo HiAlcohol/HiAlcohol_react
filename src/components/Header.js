aecc2d33a2d327a23353ee6424e01ad7ffdd2b7c
alreadynyeong <kjw87130@naver.com>
2022-06-29 20:42:05 +0900

    #2 헤더 로고 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../scss/Header.scss'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div className='Header'>
-            <Link to='/'>Hi Alcohol</Link>
+            <Link to='/'><p id='HeaderLogo'>Hi Alcohol</p></Link>
         </div>
     );
 };
