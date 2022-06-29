14d63e90429f21a23c5079d301887c6971f66366
Ahn YoungSeon <dudtjs0920@naver.com>
2022-06-30 00:33:46 +0900

    [feat] HALC-81 게시글 리스트 페이지 화면 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SearchList from './pages/SearchListPage';
 import BoardWrite from "./pages/BoardWritePage";
 import Map from "./pages/MapPage";
 import Recipe from "./pages/RecipePage"
+import Boards from "./pages/BoardListPage"
 
 const App = () => {
   return (
@@ -13,7 +14,7 @@ const App = () => {
       <Route path="/recipe" element={<Recipe />} />
 
       <Route path="/map" element={<Map />} />
-
+      <Route path="/boards" element={<Boards />}/>
       <Route path="/board/write" element={<BoardWrite />} />
 
     </Routes>
