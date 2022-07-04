65dc237dc78bc68aafebec7e32ab5d1e1729ece0
alreadynyeong <kjw87130@naver.com>
2022-07-04 12:21:34 +0900

    #6 css 적용

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,40 @@
 import ReactModal from 'react-modal';
+import Recipe from './RecipeTemplate';
+
+const ModalStyle = {
+    overlay: {
+		position: "fixed",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		backgroundColor: "rgba(255, 255, 255, 0.45)",
+		zIndex: 10,
+	},
+	content: {
+		display: "flex",
+		justifyContent: "center",
+		background: "#242424",
+		overflow: "auto",
+		top: "15vh",
+		left: "15vw",
+		right: "15vw",
+		bottom: "15vh",
+		WebkitOverflowScrolling: "touch",
+		borderRadius: "20px",
+		outline: "none",
+		zIndex: 10,
+	},
+};
 
 const Modal = ({ isOpen }) => {
+    
   return (
-    <ReactModal isOpen={isOpen}>
-      <div>모달 입니다.</div>
+    <ReactModal 
+    isOpen={isOpen}
+    style = {ModalStyle}
+    >
+      <Recipe />
     </ReactModal>
   );
 };
