934fbe74f5021b93e6adce81f1ccf6db7cc02381
alreadynyeong <kjw87130@naver.com>
2022-07-12 14:07:44 +0900

    HALC-96 검색기능 추가

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -43,19 +43,37 @@ const MapContainer = () => {
             //    new kakao.maps.InfoWindow({ position: center, map: map, content: '현재 위치'});
                var ps = new kakao.maps.services.Places(); 
                
-            //   ${a}
-            ps.keywordSearch('주류유통', placesSearchCB, {
-                location: center, 
-                sort: kakao.maps.services.SortBy.DISTANCE
-            }); 
-         
+           
+            console.log("hi >>>>", loc)
+
+             ps.keywordSearch(loc+'주류유통', placesSearchCB); 
+        
             function placesSearchCB (data, status, pagination) {
                 if (status === kakao.maps.services.Status.OK) {
+            
+                    var bounds = new kakao.maps.LatLngBounds();
+            
                     for (var i=0; i<data.length; i++) {
-                        displayMarker(data[i]);
-                    }
-                }
+                        displayMarker(data[i]);    
+                        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+                    } 
+            
+                    map.setBounds(bounds);
+                } 
             }
+
+            // ps.keywordSearch('주류유통', placesSearchCB, {
+            //     location: center, 
+            //     sort: kakao.maps.services.SortBy.DISTANCE
+            // }); 
+         
+            // function placesSearchCB (data, status, pagination) {
+            //     if (status === kakao.maps.services.Status.OK) {
+            //         for (var i=0; i<data.length; i++) {
+            //             displayMarker(data[i]);
+            //         }
+            //     }
+            // }
            
            function displayMarker(place) {
         
