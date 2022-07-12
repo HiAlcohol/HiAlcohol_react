6b040d94af5644d6e216475fa8a790874bc7f487
AYoungSn <dudtjs0920@naver.com>
2022-07-12 21:21:37 +0900

    [fix] 홈페이지 칵테일 검색 박스 레이아웃 수정

--- src/components/SearchTemplate.js
+++ src/components/SearchTemplate.js
@@ -1,15 +1,20 @@
 import '../scss/SearchTemplate.scss';
 import {FaSearch} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SearchList = () => {
     return (
         <div className="SearchListTemplate">
             <div className='search'>
                 <input type='text' placeholder="술 이름을 입력해주세요." />
-                <button type='submit'><Link to = '/searchlist'><FaSearch /></Link>
+                {/* <button type='submit'><Link to = '/searchlist'><FaSearch /></Link>
+                </button> */}
+                <button type='submit'><Link to = '/searchlist'>
+				<FontAwesomeIcon icon={faMagnifyingGlass} />
+				</Link>
                 </button>
-                
             </div>
             <br></br>
         </div>
