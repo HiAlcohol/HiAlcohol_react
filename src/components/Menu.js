d68e3ff8b57a5233aad9559873622bf5ad288278
AYoungSn <dudtjs0920@naver.com>
2022-07-13 15:01:10 +0900

    [fix] HALC-97 menu 버튼 두께 수정

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -28,8 +28,7 @@ import {slide as Slide} from 'react-burger-menu'
 
 function Menu(props) {
 	return (
-		// <div >
-		<Slide className='menu_btn' height={'10%'}>
+		<Slide className='menu_btn'>
 			<a className="menu-item" href="/boards">
 				꿀조합 게시판
 			</a>
@@ -37,7 +36,6 @@ function Menu(props) {
 				우리동네 주류매장
 			</a>
 		</Slide>
-		// </div>
 	)
 }
 
