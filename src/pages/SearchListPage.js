1660bfff225c01774ddbfe59775ec4b254fe5566
alreadynyeong <kjw87130@naver.com>
2022-07-13 16:23:32 +0900

    HALC-127 레시피 코드 수정

--- src/pages/SearchListPage.js
+++ src/pages/SearchListPage.js
@@ -2,16 +2,19 @@ import SearchListTemplate from "../components/SearchTemplate";
 import SearchListItem from "../components/SearchListItem";
 import Header from "../components/Header";
 
-const SearchListPage = () => {
-    return (
-        <>
+function SearchListPage () {
+    const recipe = [
+        {id:1, cocktail: '마가리타', rate: '1 : 1 : 1', content:'설명입니다.', img : '../img/cocktail.png'},
+        {id:2, cocktail: '마가리타2', rate: '1 : 1 : 2', content:'설명입니다2.',img : '../img/cocktail.png'}
+    ]
+    return <div>
             <Header />
             <SearchListTemplate />
-            <SearchListItem />
-            <SearchListItem />
-        </>
+            <SearchListItem recipe={recipe}></SearchListItem>
+        </div>
         
-    )
+    
 }
 
 export default SearchListPage;
+
