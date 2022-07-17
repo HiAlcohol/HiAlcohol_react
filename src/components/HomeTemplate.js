32ffe4eacfa4b06aaa4bd8d985077ce84163e04f
alreadynyeong <kjw87130@naver.com>
2022-07-17 14:37:23 +0900

    HALCL-135 [feat] 오늘의 술 추천 모달창 구현

--- src/components/HomeTemplate.js
+++ src/components/HomeTemplate.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import {FaSearch} from 'react-icons/fa';
+import Modal from 'react-modal';
 import Search from './SearchTemplate';
 import '../scss/HomeTemplate.scss';
 import cocktail from '../img/cocktail.png';
@@ -8,7 +9,51 @@ import Header from './Header';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMartiniGlass, faMartiniGlassCitrus } from "@fortawesome/free-solid-svg-icons";
 
-const HomeTemplate = () => {
+function HomeTemplate(props) {
+
+	const recipe = props.recipe;
+	const materials = recipe.materials;
+	const inputItem = []
+        for (let j = 0; j<3; j++){
+            inputItem.push(
+                <div className='in'>{materials[j]}</div>
+            )
+        }
+	const [isOpen, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(true);
+      };
+      const handleClickCancle = () => {
+        setOpen(false);
+      }
+
+	  const ModalStyle = {
+		overlay: {
+			position: "fixed",
+			top: 0,
+			left: 0,
+			right: 0,
+			bottom: 0,
+			backgroundColor: "rgba(255, 255, 255, 0.45)",
+			zIndex: 10,
+		},
+		content: {
+			display: "flex",
+			justifyContent: "center",
+			background: "#242424",
+			overflow: "auto",
+			top: "15vh",
+			left: "15vw",
+			right: "15vw",
+			bottom: "15vh",
+			WebkitOverflowScrolling: "touch",
+			borderRadius: "20px",
+			outline: "none",
+			zIndex: 10,
+		},
+	};
+
     return (
 
 		<>
@@ -26,7 +71,30 @@ const HomeTemplate = () => {
 				<div className='recommend'>
 					<p>오늘의 술 추천</p>
 					
-					<a>마가리타 {'>'}</a>
+					<a onClick={handleClick}>{recipe.cocktail} {'>'}</a>
+					<Modal isOpen={isOpen} 
+                    style={ModalStyle}
+                    onRequestClose={handleClickCancle}
+                >   
+                    <div className='RecipeTemplate'>
+                        <button id='recipe-close' onClick={handleClickCancle}>X</button>
+                        <br /><br /><br />
+						<div class="name">{recipe.cocktail}</div>
+						<br />
+						<img src = {recipe.img} className="recipe-img" />
+						<br /><br />
+						<div className="material">
+								{inputItem}
+						</div>
+						<br /><br /><br />
+						<p>- 비율 -</p>
+						<h2>{recipe.rate}</h2>
+						<br /><br />
+						<p>{recipe.content}</p>
+					</div>
+                </Modal>
+					
+                 
 					<br></br>
 					<img src = {cocktail} className='cockimg' />
 
