5b6c4ae7e315ae915ba0854e47047dd31e2f490a
alreadynyeong <kjw87130@naver.com>
2022-07-27 12:39:01 +0900

    HALC-151 [modify] 링크 수정

--- src/components/BoardListItem.js
+++ src/components/BoardListItem.js
@@ -2,9 +2,21 @@ import heartfill from '../img/heart_fill.png'
 import heart from '../img/heart_outline.png'
 import '../scss/board/BoardListItem.scss'
 
-function Item({board}) {
+function Item(props) {
+    const board = props.board
+    const link = props.link
+    console.log("what", {link})
+    let linkIs = ""
+    if (link === 'board'){
+        linkIs = "/board/" 
+    }
+    else if(link === 'suggestion') {
+        console.log("ok")
+        linkIs = "/suggestions/" 
+    }
+
 	return <div className="content">
-		<a href={"/board/" + board.postId}>
+		<a href={linkIs + board.postId}>
 			<div className="subject">
 				<p>{board.title}</p>
 				<div className="info">
@@ -26,50 +38,11 @@ function Item({board}) {
 function BoardListItem(props) {
 
     let board = props.board;
+    let link = props.link
 
     return <div className='contentList'>
-		{board.map(board => (<Item board={board} key={board.postId}/>))}
+		{board.map(board => (<Item board={board} link= {link} key={board.postId}/>))}
 	</div>
-
-    // let linkIs = ""
-    // if (props.link === 'board'){
-    //     linkIs = "/board/" 
-    // }
-    // else if(props.link === 'suggestion') {
-    //     console.log("ok")
-    //     linkIs = "/suggestions/" 
-    //     console.log("?? >>", linkIs)
-    // }
-    
-    // const board = props.board;
-    
-    
-    // const listItem = []
-    // for (let i = 0; i < props.board.length; i++) {
-        
-    //     listItem.push(
-    //         <div className="content">
-    //             <a href={linkIs+ board[i].id}>
-    //                 <div className="subject">
-    //                     <p>{board[i].title}</p>
-    //                     <div className="info">
-    //                         <span>{board[i].userName}</span>
-    //                         <span> | </span>
-    //                         <span>{board[i].createDate}</span>
-    //                     </div>
-    //                 </div>
-    //             </a>
-    //             <div className="like">
-    //                 <a href="#"><button id="img_btn" className="likebtn">
-    //                     <input type="image" id="likeImg" src={heart} />
-    //                 </button></a>
-    //                 <div id="likes" disabled="disabled">{board[i].likedCnt}</div>
-    //             </div>
-    //         </div>
-    //     )
-    // }
-    // return <div className='contentList'>{listItem}</div>
-
     
 }
 
