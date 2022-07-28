2bd06f6fc85f8d33a86f0b3d7d083ce1ddfe0702
alreadynyeong <kjw87130@naver.com>
2022-07-28 12:11:50 +0900

    HALC-163 신고된 게시글 조회를 위한 임시 코드 작성

--- src/pages/admin/AdminReportPage.js
+++ src/pages/admin/AdminReportPage.js
@@ -1,7 +1,43 @@
 import Header from '../../components/Header'
 import ReportBoards from '../../components/admin/ReportBoards'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function AdminReport() {
+	const [reports, setReports] = useState(null);
+	const [reportsComent, setReportsComent] = useState(null);
+	const [error, setError] = useState(null);
+
+	useEffect( () => {
+		const fetchReports = async () => {
+
+			try {
+				console.log('렌더링이 완료되었습니다!');
+				const response = await axios.get(
+					'http://3.35.208.41:5000/admin/reports/board'
+				);
+				setReports(response.data.data);
+			} catch(e) {
+				setError(e)
+			}
+		};
+		const fetchReportsComent = async () => {
+
+			try {
+				console.log('렌더링이 완료되었습니다!');
+				const response = await axios.get(
+					'http://3.35.208.41:5000/admin/reports/board'
+				);
+				setReports(response.data.data);
+			} catch(e) {
+				setError(e)
+			}
+		};
+		fetchReportsComent();
+		
+	}, []);
+
+
 	const dummy = [
 		{key: 1, id:1, title: '잭콕', userName: '유저1', createDate: '2021.10.15', visible: true},
 		{key: 2, id:2, title: '잭콕', userName: '유저1', createDate: '2021.10.15', visible: false},
