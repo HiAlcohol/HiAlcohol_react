53945080c28f36a37cce38f928d1505d2b6cac56
AYoungSn <dudtjs0920@naver.com>
2022-08-03 20:50:56 +0900

    [feat] HALC-145 내가 쓴 꿀조합 게시글 모아보기 기능

--- src/pages/board/MyBoardPage.js
+++ src/pages/board/MyBoardPage.js
@@ -1,11 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import BoardListItem from "../../components/BoardListItem";
 import Header from "../../components/Header";
 
 function MyBoards() {
-    const board = [
-        {id: 1, title: '알쓰를 위한 편의점 칵테일', userName: '유저', createDate: '2022.03.14', likedCnt: 8},
-        {id: 2, title: '술 테스트 게시글', userName: '유저2', createDate: '2022.03.15', likedCnt: 2}
-    ]
+	const [board, setBoard] = useState(null);
+	const [error, setError] = useState(null)
+	console.log(1)
+	useEffect(() => {
+		const fetchBoard = async () => {
+			console.log(1);
+			try {
+				if (localStorage.getItem("token")) {
+					const response = await axios.get('http://3.35.208.41:5000/users/boards', 
+					{headers: {
+						Authorization: `Bearer ${localStorage.getItem("token")}`,
+					}
+					});
+					console.log(response.data.data)
+					setBoard(response.data.data)
+				}
+			} catch(e) {
+				setError(e)
+			}
+		}
+		fetchBoard();
+	}, [])
+    // const board = [
+    //     {id: 1, title: '알쓰를 위한 편의점 칵테일', userName: '유저', createDate: '2022.03.14', likedCnt: 8},
+    //     {id: 2, title: '술 테스트 게시글', userName: '유저2', createDate: '2022.03.15', likedCnt: 2}
+    // ]
+	console.log(1)
+	if (error) return <div>{error}</div>
+	const list = (board) ?<BoardListItem board={board}/>:<p>data 없음</p>
     return <div>
         <Header right="board"></Header>
         <div className="dropdown">
@@ -16,7 +43,7 @@ function MyBoards() {
                 </select>
             </form>
         </div>
-        <BoardListItem board={board}></BoardListItem>
+        {list}
     </div>
 }
 
