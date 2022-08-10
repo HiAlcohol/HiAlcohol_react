f76a9dd01b0245b4615dcaf7bee9da9d755c5ac1
alreadynyeong <kjw87130@naver.com>
2022-08-10 10:33:20 +0900

    HALC-154 레시피 추가 페이지 body 설정

--- src/pages/admin/AddRecipe.js
+++ src/pages/admin/AddRecipe.js
@@ -1,9 +1,57 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import axios from "axios";
 import Header from '../../components/Header'
 import "../../scss/admin/AddRecipe.scss"
 
 function AddRecipeTemplate() {
+	const [cocktail, SetCocktail] = useState("")
+	const [rate, SetRate] = useState("")
+    const [content, SetContent] = useState("")
+	const [material, SetMaterial] = useState("")
+	const [materials, SetMaterials] = useState("")
+
+    const cocktailHandler = (e) => {
+        e.preventDefault();
+        SetCocktail(e.target.value);
+    }
+	const RateHandler = (e) => {
+        e.preventDefault();
+        SetRate(e.target.value);
+    }
+    const contentHandler = (e) => {
+        e.preventDefault();
+        SetContent(e.target.value);
+    }
+	const materialHandler = (e) => {
+        e.preventDefault();
+        SetMaterial(e.target.value)
+    }
+
+	const cancleEvent = () => {
+		window.location.replace("/admin/cocktail")
+	}
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        let body = {
+            cocktail : cocktail,
+ 			materials: material.split(', '),
+  			rate: rate,
+  			content: content
+        };
+		console.log("??", body)
+    
+        // axios.post('http://43.200.182.67:5000/admin/recipe', body,
+        // {headers: {
+		// 	Authorization: `Bearer ${localStorage.getItem("token")}`,
+		//   }
+		// })
+        // .then((res) => console.log(res));
+		// window.location.replace("/admin/cocktail");
+        
+    }
 
     return (
 
@@ -14,7 +62,7 @@ function AddRecipeTemplate() {
         <table className="input_box">
 			<tr className="cockname_box">
 				<th><p>칵테일 이름</p></th>
-				<td><input type="text"  /></td>
+				<td><input type="text"  onChange={cocktailHandler}/></td>
 			</tr>
             <tr className="alcotype_box">
 				<th><p>술종류</p></th>
@@ -26,8 +74,10 @@ function AddRecipeTemplate() {
 			</tr>
             <tr className="cockinput_box">
 				<th><p>재료</p></th>
-				<td><input type="text" />
-				<button id="inputbtn" type="submit">재료추가+</button></td>
+				<td><input type="text" onChange={materialHandler}/>
+
+				{/* <button id="inputbtn" type="submit" onClick={addmaterial(material)}>재료추가+</button> */}
+				</td>
 				
 			</tr>
 			<tr>
@@ -35,11 +85,11 @@ function AddRecipeTemplate() {
 			</tr>
             <tr className="cockrate_box">
 				<th><p>비율</p></th>
-				<td><input type="text"  /></td>
+				<td><input type="text"  onChange={RateHandler}/></td>
 			</tr>
             <tr className="cockncontent_box">
 				<th><p>설명</p></th>
-				<td><input type="text" /></td>
+				<td><input type="text" onChange={contentHandler}/></td>
 			</tr>
 			<tr className="cockimg">
 				<th><p>칵테일 사진 업로드 +</p></th>
@@ -47,8 +97,8 @@ function AddRecipeTemplate() {
 			</tr>
 		</table>
 		<div className="btnzone">
-			<button id = "canclebtn">취소</button>
-			<button id = "okbtn">확인</button>
+			<button id = "canclebtn" onClick={cancleEvent}>취소</button>
+			<button id = "okbtn" onClick={submitHandler}>확인</button>
 		</div>
 
         
