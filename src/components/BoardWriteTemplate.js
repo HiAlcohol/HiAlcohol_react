8e6b60f35fe3e5dd94414a82118de10f54205a49
AYoungSn <dudtjs0920@naver.com>
2022-08-11 20:30:27 +0900

    [feat] HALC-154 글 작성 api 연동

--- src/components/BoardWriteTemplate.js
+++ src/components/BoardWriteTemplate.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../scss/board/BoardWriteTemplate.scss';
 import Header from './Header';
 
 const BoardWriteTemplate = () => {
+	const [board, setBoard] = useState({
+		title: "",
+		content: "",
+		images: []
+	})
+	const onChangeBoard = (e) => {
+		setBoard({
+			...board,
+			[e.target.name]: e.target.value
+		})
+	}
     return (
         <div className='BoardWriteTemplate'>
            <form>
             <div className='main-title'>
-                <Header right='write'></Header>
+                <Header right='write' board={board}></Header>
                 <div className='container'>
                     <div className='writeTitle'>
-                        <input type='text' placeholder='제목'></input>
+                        <input type='text' name="title" placeholder='제목' onChange={onChangeBoard}></input>
                     </div>
                     <div className='contents'>
-                        <textarea name='content' placeholder='내용 입력'></textarea>
+                        <textarea name='content' placeholder='내용 입력' onChange={onChangeBoard}></textarea>
                     </div>
                 </div>
             </div>
