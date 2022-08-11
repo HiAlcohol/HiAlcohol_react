abf1937759f043d3897bc5254a9167e35f6b15c7
alreadynyeong <kjw87130@naver.com>
2022-08-11 22:30:45 +0900

    HALC-168 관리자 게시글 숨김 연동

--- src/components/admin/ReportBoards.js
+++ src/components/admin/ReportBoards.js
@@ -1,7 +1,21 @@
 import '../../scss/board/BoardListItem.scss'
 import '../../scss/admin/Admin.scss'
+import axios from "axios";
 
 function ListItem({board}) {
+	const reportHandler = (e) => {
+        e.preventDefault();
+
+        axios.patch('http://43.200.182.67:5000/admin/reports/board/'+board.id, null,
+        {headers: {
+			Authorization: `Bearer ${localStorage.getItem("token")}`,
+		  }
+		})
+        .then((res) => console.log(res));
+        
+    }
+
+
 	return <div className="content">
 			<a href={"/board/" + board.id}>
 				<div className="subject">
@@ -13,7 +27,7 @@ function ListItem({board}) {
 					</div>
 				</div>
 			</a>
-			<div className='visible'>{board.visible ? '숨김': '해제'}</div>
+			<div className='visible' onClick={reportHandler}>{board.visible ? '숨김': '해제'}</div>
 		</div>
 }
 
