9e9029cb8724c59eb7501254d6dc7002cf392c7c
AYoungSn <dudtjs0920@naver.com>
2022-08-13 20:23:40 +0900

    [feat] HALC-150 유저 프로필 이미지 업로드 기능 추가

--- src/pages/NickNameEditPage.js
+++ src/pages/NickNameEditPage.js
@@ -28,7 +28,9 @@ const Confirm = styled.button`
 `
 function NicknameEdit() {
 	const [error, setError] = useState(null);
-	const [user, setUser] = useState({})
+	const [nick, setNick] = useState('');
+	const [image, setImage] = useState({});
+	const [imgBase64, setImageBase64] = useState({});
 	const dummyUser = { profile: 'http://k.kakaocdn.net/dn/dotDrj/btrybGQioYJ/xyy5MXVs2ESIAodvdnZIw1/img_110x110.jpg',
 						nickname: 'user'}
 	useEffect(() => {
@@ -39,11 +41,9 @@ function NicknameEdit() {
 					Authorization: `Bearer ${localStorage.getItem("token")}`,
 				  }
 				})
-				setUser({
-					nickname: response.data.data.nickname,
-					profile_url: response.data.data.profile_url
-				})
-				console.log('profile_url', response.data.data.profile_url)
+				setNick(response.data.data.nickname)
+				setImage(response.data.data.profile_url)
+				
 			} catch(e) {
 				setError(e);
 			}
@@ -51,55 +51,74 @@ function NicknameEdit() {
 		initUser();
 	},[])
 
-	const onChangeUser = (e) => {
-		setUser({
-			...user,
-			[e.target.name]: e.target.value
-		})
+	const onChangeNickname = (e) => {
+		setNick(e.target.value);
+	}
+
+	const onChangeImage = (e) => {
+		// setImage(e.target.files[0])
+		const img = e.target.files[0];
+		const formData = new FormData();
+		formData.append('profile_url', img)
+		setImage(img)
+		console.log('e.target.files', e.target.files)
+		setImageBase64([])
+		let reader = new FileReader();
+		reader.readAsDataURL(e.target.files[0]);
+		reader.onloadend = () => {
+			const base64 = reader.result;
+			console.log(base64);
+			if (base64) {
+				var base64Sub = base64.toString();
+				setImageBase64(imgBase64 => [...imgBase64, base64Sub]);
+			}
+		}
 	}
 
 	const confirm = async (e) => {
 		e.preventDefault();
 		console.log(1)
+		const formData = new FormData();
+		formData.append('profile_url', image)
+		console.log('formdata', formData)
 		try {
-			const response = await axios.put('http://43.200.182.67:5000/users',
-				{nickname: user.nickname},
+			await axios.put('http://43.200.182.67:5000/users',
+				{nickname: nick},
 				{headers: {
 					Authorization: `Bearer ${localStorage.getItem("token")}`,
 				}
 				})
-			setUser({
-				...user,
-				nickname: response.data.data.nickname
-			})
-			console.log('res1:',user)
-			const response2 = await axios.put('http://43.200.182.67:5000/users/image',
-				{profile_url: user.profile_url},
+		} catch(e) {
+			setError(e);
+			console.log('닉네임 수정 오류')
+		}
+		try {
+			const response2 = await axios.post('http://43.200.182.67:5000/users/image',
+				formData,
 				{headers: {
 					Authorization: `Bearer ${localStorage.getItem("token")}`,
+					"Content-Type": `multipart/form-data;`,
 				  }
 				});
-			setUser({
-				...user,
-				profile_url: response2.data.data.profile_url
-			})
+			setImage(response2.data.data.profile_url)
 		}catch(e) {
 			setError(e)
+			console.log('프로필 사진 업로드 오류')
 		}
 	}
-	
+	if (error) return <p>{error}</p>
 	return <>
 		<Header right='common'></Header>
 		<p className="nick_edit_header">회원정보 수정</p>
 		<table className="input_box">
 			<tr className="nickname_box">
 				<th><p>닉네임</p></th>
-				<td><input type="text" name="nickname" onChange={onChangeUser} defaultValue={user.nickname}/></td>
+				<td><input type="text" name="nickname" onChange={onChangeNickname} defaultValue={nick}/></td>
 			</tr>
 			<tr className="profile">
 				<th><p>프로필 사진</p></th>
 				<td>
-					<input type='file' name="profile_url" accept='image/*' onChange={onChangeUser} defaultValue={user.profile}/>
+					<input type='file' name="profile_url" accept='image/*' onChange={onChangeImage} />
 				</td>
 			</tr>
 		</table>
